b5a4884d87d5cf67e4437c5940dfceca27628429
Ikram Hamizi <36704227+ikram-hamizi@users.noreply.github.com>
2021-04-13 22:56:03 +0300

    Update preprocess.py

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -14,7 +14,7 @@ def name_to_vector(name, vocab, name_maxlen):
   return vector
 
 # function: convert the list-of-names data to NumPy arrays of integer indices to be fed to the models. The arrays are post-padded.
-def data_to_matrix(names, matrix_size, vocab, name_maxlen)
+def data_to_matrix(names, matrix_size, vocab, name_maxlen):
   matrix = np.zeros((matrix_size, name_maxlen))  
   for i, name in enumerate(names):
     matrix[i] = name_to_vector(name, vocab, name_maxlen)
