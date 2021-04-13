5b56259c1b3a6d1b8691ab83d1438dd79510e628
Ikram Hamizi <36704227+ikram-hamizi@users.noreply.github.com>
2021-04-13 23:12:14 +0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,7 +1,44 @@
+#local classes
 from context import scripts
 import scripts
-from scripts.load_GENDER import get_data
+from scripts.dataExtract import get_data
+from scripts.preprocess import data_to_matrix
+
+#python libraries
+import pandas as pd
+import numpy as np
 from keras.models import load_model
+
+#Load data
+train, test = get_data() #csv english
+
+x_test = test['Name']
+y_test = test['Gender'].values.tolist() 
+
+
+#Preprocess data
+#Encode labels with integer representations
+from sklearn.preprocessing import LabelEncoder
+
+# intermediate step: get label encodings from training set labels
+# Sort names by length before creating mini-batches to help the model to learn shorter sequences first.
+s = train.Name.str.len().sort_values().index #Int64Index
+train = train.reindex(s)
+
+y_train = train['Gender'].values.tolist()
+y_train = np.array(label_encoder.fit_transform(y_train))
+
+label_encoder = LabelEncoder()
+y_test = np.array(label_encoder.transform(y_test))
+
+
+# Get Matrix of vector representation of names
+total_test = len(test) #83288
+total_vocab = len(unique)+2 #52+2
+vocab_size = len(vocab) #52
+name_maxlen = 15
+
+matrix_train_y = data_to_matrix(x_test, total_test, vocab, name_maxlen):
 
 
 model1 = load_model('baselneLSTM.h5')
