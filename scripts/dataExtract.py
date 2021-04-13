2e3508590160d7dbfaa187908dff92e95c3d1d8d
Ikram Hamizi <36704227+ikram-hamizi@users.noreply.github.com>
2021-04-13 23:25:17 +0300

    Update dataExtract.py

--- scripts/dataExtract.py
+++ scripts/dataExtract.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
-def get_data(lang="eng"):
-
-
-	train = pd.read_csv("train_eng.csv") if lang == "eng" else pd.read_csv("train_rus.csv")
-	test = pd.read_csv("test_eng.csv") if lang == "eng" else pd.read_csv("test_rus.csv")
-
+def get_data(path="../data/", lang="eng"):
 	
+	assert path[-1]=="/", f"Path Error '{path}': Your path should end in a '/': e.g. '../data/'"
+	
+	train_path = path = "train_eng.csv"
+	test_path = path "test_eng.csv"
+	
+	train = pd.read_csv(train_path) if lang == "eng" else pd.read_csv("train_rus.csv")
+	test = pd.read_csv(test_path) if lang == "eng" else pd.read_csv("test_rus.csv")
+
 	return train, test
